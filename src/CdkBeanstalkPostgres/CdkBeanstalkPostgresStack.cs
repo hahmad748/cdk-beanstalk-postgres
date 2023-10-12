0e58808ac99c2bac34f4960cc6973525c97c787c
Haseeb Ahmad <hahmad748@gmail.com>
2023-10-13 02:07:29 +0500

    Initial commit

---
+++ src/CdkBeanstalkPostgres/CdkBeanstalkPostgresStack.cs
@@ -0,0 +1,13 @@
+using Amazon.CDK;
+using Constructs;
+
+namespace CdkBeanstalkPostgres
+{
+    public class CdkBeanstalkPostgresStack : Stack
+    {
+        internal CdkBeanstalkPostgresStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
